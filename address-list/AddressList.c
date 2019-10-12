dd2f0f12c62d61c9700bae94c6ddb76f914c9866
famur <1779936164@qq.com>
2019-10-12 08:36:05 -0700

    second add code

--- address-list/AddressList.c
+++ address-list/AddressList.c
@@ -67,3 +67,62 @@ void AddContact(contact*pcon)
     printf("添加成功\n");
 }
 
+void ShowContact( contact*pcon)
+{
+    int i = 0;
+    printf("%-10s\t%-5s\t%-5s\t%-12s\t%-20s\n", "姓名", "电话号码", "分类", "电子邮件地址", "条目编号");
+    for (i = 0; i < pcon->size; i++)
+    {
+        printf("%-10s\t%-12s\t%-5d\t%-12s\t%-5d\n",
+                pcon->data[i].name,
+                pcon->data[i].tele,
+                pcon->data[i].type,
+                pcon->data[i].email,
+                pcon->data[i].num);
+    }
+}
+
+int Search(contact*pcon, char name[])
+{
+    int i = 0;
+    for (i = 0; i < pcon->size; i++)
+    {
+        if (strcmp(pcon->data[i].name,name)==0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void DelContact(contact*pcon)
+{
+    if (pcon->size == 0)
+    {
+        printf("通讯录已空无法删除\n");
+    }
+    else
+    {
+        char name[MAX] = { 0 };
+        printf("请输入姓名：>");
+        scanf("%s", name);
+        int t = Search(pcon, name);
+        if (t == -1)
+        {
+            printf("输入姓名有误无法删除\n");
+        }
+        else
+        {
+            int i = 0;
+            {
+                for (i = t; i < pcon->size - 1; i++)
+                {
+                    pcon->data[i] = pcon->data[i + 1];
+                }
+                pcon->size--;
+                printf("删除成功\n");
+            }
+        }
+    }
+    
+}
